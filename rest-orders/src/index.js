bab29bdd82fc11d8c0c3ed11d9251f35de06a3dc
fernando-apollo <fernando@apollographql.com>
2023-01-31 16:59:18 +0000

    updated

--- rest-orders/src/index.js
+++ rest-orders/src/index.js
@@ -1,18 +1,18 @@
-const gql = require("graphql-tag");
-const { readFileSync } = require("fs");
-const { ApolloServer } = require("@apollo/server");
-const { buildSubgraphSchema } = require("@apollo/subgraph");
-const { startStandaloneServer } = require("@apollo/server/standalone");
+const gql = require('graphql-tag');
+const { readFileSync } = require('fs');
+const { ApolloServer } = require('@apollo/server');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
+const { startStandaloneServer } = require('@apollo/server/standalone');
 
-const resolvers = require("./resolvers");
-const OrdersAPI = require("./datasources/orders-api");
+const resolvers = require('./resolvers');
+const OrdersAPI = require('./datasources/orders-api');
 const port = process.env.PORT ?? 4002;
-const subgraphName = require("../package.json").name;
+const subgraphName = require('../package.json').name;
 
 async function main() {
   const typeDefs = gql(
-    readFileSync("schema.graphql", {
-      encoding: "utf-8",
+    readFileSync('schema.graphql', {
+      encoding: 'utf-8',
     })
   );
   const server = new ApolloServer({
@@ -21,6 +21,9 @@ async function main() {
   const { url } = await startStandaloneServer(server, {
     // @WORKSHOP: 2.2.3 - Pass orders API through context
     context: async ({ req }) => ({
+      dataSources: {
+        ordersAPI: new OrdersAPI(),
+      },
     }),
     listen: { port },
   });
--- rest-orders/src/resolvers/index.js
+++ rest-orders/src/resolvers/index.js
@@ -1,13 +1,13 @@
 const resolvers = {
   Query: {
-    // @WORKSHOP 2.3.1: Create a Query resolver for order operation and return an order
-    order() {
-      return 
-    }
+    order: (_, { id }, { dataSources }) => dataSources.ordersAPI.getOrder(id),
   },
-  // @WORKSHOP 2.3.2: Add a reference resolver for Order Entity
   Order: {
-  }
+    __resolveReference: (order, { dataSources }) =>
+      dataSources.ordersAPI.getOrder(order.id),
+    buyer: (root) => ({ id: root.customerId }),
+    items: (root) => root.variantIds.map((variantId) => ({ id: variantId })),
+  },
 };
 
 module.exports = resolvers;
